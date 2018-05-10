e37c775c5cd7eeed1e9d57d7a8689ccc8ce7d7ce
Alexander García <halexgs1987@gmail.com>
2018-05-10 18:09:09 -0500

    Modificando campos en el modelos de Asesor y arreglando el bug del controlador de Asesor

--- app/SubSubTipoActividad.php
+++ app/SubSubTipoActividad.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class SubSubTipoActividad extends Model
 {
-    protected $table = 'subysubsubtipo';
-    //protected $table = 'subsubtipo_actividad';
+    //protected $table = 'subysubsubtipo';
+    protected $table = 'subsubtipo_actividad';
 
     protected $fillable = [
         'id', 'crmit_subsubname', 'crmit_subtipoactividadid', 'crmit_codigounico', 'crmit_subname'
